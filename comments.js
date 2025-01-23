8f70d6aee8808d34003fa2dfe52fc9c355339748
pcipera <43580295+pcipera@users.noreply.github.com>
2025-01-23 02:33:59 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,8 +1,4 @@
-// Create a web server that listens on port 3000 and serves the comments.html file. Use the comments.html file from the previous exercise.
-// When you visit http://localhost:3000/, the comments.html file should be served.
-// When you visit http://localhost:3000/favicon.ico, the web server should respond with a 404 status code.
-// When you visit http://localhost:3000/anyotherpath, the web server should respond with a 404 status code.
-
+// Create a web server
 const http = require('http');
 const fs = require('fs');
 
